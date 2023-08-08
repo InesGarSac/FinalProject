d7f695e69ccfc7c26c494c1dc8344b6d8f2cb068
Ines Garcia <garciasacines@gmail.com>
2023-08-08 08:26:05 +0200

    Add CircularProgressBar to Movie List Screen

--- app/src/main/java/com/keepcoding/finalproject/presentation/list/moviesList/MovieListScreen.kt
+++ app/src/main/java/com/keepcoding/finalproject/presentation/list/moviesList/MovieListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import com.keepcoding.finalproject.components.CircularIndeterminateProgressBar
 import org.koin.androidx.compose.koinViewModel
 
 
@@ -16,12 +17,14 @@ fun MovieListScreen(
 ) {
 
     val state = movieListViewModel.movieList.observeAsState()
+    val movieList = state.value
     // Get data from ViewModel
 
+    if (movieList == null) CircularIndeterminateProgressBar(isDisplayed = true)
+    
     LazyColumn(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val movieList = state.value
 
         items(movieList?.size ?: 0) { i ->
             val item = movieList?.get(i)
